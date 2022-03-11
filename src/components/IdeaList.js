2280028ad9bda4404c752534945c36a5a295baa8
PascalPromoTrinity <lapascal89@gmail.com>
2022-03-11 14:13:44 +0100

    search by double click on badge: done

--- src/components/IdeaList.js
+++ src/components/IdeaList.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import {
   Badge,
   Button,
+  Col,
   Container,
   Form,
   FormControl,
   InputGroup,
+  Row,
   Spinner,
   Table,
 } from "react-bootstrap";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import { useAuthState } from "../context/auth";
+import { BadgePill } from "./FormInput";
 
 const IdeaList = ({ ideaId }) => {
   const { user, token } = useAuthState();
@@ -34,10 +37,12 @@ const IdeaList = ({ ideaId }) => {
     };
 
     try {
+      setSearchLoading(true);
       const { data } = await axios.get(
         "http://localhost:5000/api/ideas/idealist",
         config
       );
+      setSearchLoading(false);
       setIdeas(data);
     } catch (error) {
       console.log(error);
@@ -100,13 +105,11 @@ const IdeaList = ({ ideaId }) => {
   //* ============================== SORT BY NAME ==============================
 
   const useSortNameAsc = (arr) => {
-    const [state, setState] = useState([]);
-    return () => setState(arr.sort((a, b) => a.name.localeCompare(b.name)));
+    return () => arr.sort((a, b) => a.name.localeCompare(b.name));
   };
 
   const useSortNameDesc = (arr) => {
-    const [state, setState] = useState([]);
-    return () => setState(arr.sort((a, b) => b.name.localeCompare(a.name)));
+    return () => arr.sort((a, b) => b.name.localeCompare(a.name));
   };
 
   const sortNameAsc = useSortNameAsc(ideas);
@@ -115,50 +118,104 @@ const IdeaList = ({ ideaId }) => {
   //* ============================== SORT BY STAR ==============================
 
   const useSortStar = (arr) => {
-    const [state, setState] = useState([]);
-    return () =>
-      setState(arr.sort((a, b) => (a.star === b.star ? 0 : a.star ? -1 : 1)));
+    return () => arr.sort((a, b) => (a.star === b.star ? 0 : a.star ? -1 : 1));
   };
 
   const useUndoSortStar = (arr) => {
-    const [state, setState] = useState([]);
-    return () =>
-      setState(arr.sort((a, b) => (a.star === b.star ? 0 : b.star ? -1 : 1)));
+    return () => arr.sort((a, b) => (a.star === b.star ? 0 : b.star ? -1 : 1));
   };
 
   const sortStar = useSortStar(ideas);
   const sortUndoStar = useUndoSortStar(ideas);
 
+  //* ============================== SEARCH BY BADGE ==============================
+
+  let fetchTags = []; // Pushing the tags from ideas
+  ideas.map((data) => fetchTags.push(...data.tags));
+  const tagArray = Array.from(new Set(fetchTags)); // Removing doubles
+
+  let fetchAccess = []; // Pushing the access from ideas
+  ideas.map((data) => fetchAccess.push(...data.access));
+  const accessArray = Array.from(new Set(fetchAccess)); // Removing doubles
+
+  let fetchLang = []; // Pushing the language from ideas
+  ideas.map((data) => fetchLang.push(...data.language));
+  const langArray = Array.from(new Set(fetchLang)); // Removing doubles
+
   return (
     <>
-      <div className="d-flex pt-5 mt-5 justify-content-center">
-        <h1 style={{ color: "#4749f4" }}>
-          {user && `Bienvenue, ${user.firstname}`}
-        </h1>
-      </div>
-      <div className="d-flex mt-4 justify-content-center">
-        <div>
-          <Form.Select onChange={(e) => handleChange(e)}>
-            <option value="default">Trier par...</option>
-            {/* <option value="default">Défault</option> */}
-            <option value="sortedNameAsc">Nom de A à Z</option>
-            <option value="sortedNameDesc">Nom de Z à A</option>
-            <option value="sortedStarTrue">Stars en premiers</option>
-            <option value="sortedStarFalse">Stars en derniers</option>
-          </Form.Select>
-        </div>
-        <div>
-          <InputGroup className="mb-3">
-            <FormControl
-              onChange={(e) => handleSearch(e.target.value)}
-              placeholder="Recherche par mot-clé..."
-            />
-          </InputGroup>
-        </div>
-        <Link to="/newIdea">
-          <Button>+ Nouvelle idée</Button>
-        </Link>
-      </div>
+      <Container>
+        <Col>
+          <Col className="d-flex pt-5 mt-5">
+            <h1 style={{ color: "#4749f4" }}>
+              {user && `Bienvenue, ${user.firstname}`}
+            </h1>
+          </Col>
+          <Row className="mt-5">
+            <Col>
+              <InputGroup className="mb-3">
+                <FormControl
+                  onChange={(e) => handleSearch(e.target.value)}
+                  placeholder="Recherche par mot-clé..."
+                />
+              </InputGroup>
+            </Col>
+            <Col>
+              <Form.Select onChange={(e) => handleChange(e)}>
+                <option value="default">Trier par...</option>
+                <option value="sortedNameAsc">Nom de A à Z</option>
+                <option value="sortedNameDesc">Nom de Z à A</option>
+                <option value="sortedStarTrue">Stars en premiers</option>
+                <option value="sortedStarFalse">Stars en derniers</option>
+              </Form.Select>
+            </Col>
+            <Col xs={2}>
+              <Link to="/newIdea">
+                <Button>+ Nouvelle idée</Button>
+              </Link>
+            </Col>
+            <Col xs={2}>
+              <a href="/">
+                <Button variant="danger">Réinitialiser</Button>
+              </a>
+            </Col>
+          </Row>
+          <Row className="my-5">
+            <Col xs={8}>
+              {tagArray.map((tag, index) => (
+                <BadgePill
+                  key={index}
+                  value={tag}
+                  bg="success"
+                  onClick={() => handleSearch(tag)}
+                />
+              ))}
+            </Col>
+            <Col xs={2}>
+              {accessArray.map((access, index) => (
+                <BadgePill
+                  key={index}
+                  value={access}
+                  onClick={() => handleSearch(access)}
+                />
+              ))}
+            </Col>
+            <Col xs={2}>
+              {langArray.map((lang, index) => (
+                <BadgePill
+                  key={index}
+                  value={lang}
+                  bg="danger"
+                  onClick={() => handleSearch(lang)}
+                />
+              ))}
+            </Col>
+          </Row>
+        </Col>
+      </Container>
+
+      {/* //* ================ SEARCH: NO RESULT ================ */}
+
       {searchResult.length === 0 && search.length > 0 && !searchLoading ? (
         <div
           className="d-flex justify-content-center align-items-center mt-5 pt-5 text-warning"
@@ -167,6 +224,8 @@ const IdeaList = ({ ideaId }) => {
           <h1>Aucun résultat</h1>
         </div>
       ) : (
+        //* ================ LOADING WHILE SEARCHING ================ */
+
         <Container>
           {searchLoading ? (
             <div
@@ -188,15 +247,23 @@ const IdeaList = ({ ideaId }) => {
                     <th>Tags</th>
                     <th>Accès</th>
                     <th>Langue</th>
-                    <th>&#9733;</th>
+                    {/* <th>&#9733;</th> */}
                   </tr>
                 </thead>
+
+                {/* //* ================ SEARCH RESULT ================ */}
 
                 {search.length >= 1 &&
                   searchResult.map((result, index) => (
                     <tbody key={index}>
                       <tr>
-                        <td>{index + 1}</td>
+                        <td>
+                          {result.star ? (
+                            <Badge bg="warning">&#9733;</Badge>
+                          ) : (
+                            index + 1
+                          )}
+                        </td>
                         <img
                           className="rounded img-thumbnail"
                           src={result.logo}
@@ -228,11 +295,6 @@ const IdeaList = ({ ideaId }) => {
                             </Badge>
                           ))}
                         </td>
-                        {result.star ? (
-                          <td className="text-warning">&#9733;</td>
-                        ) : (
-                          <td></td>
-                        )}
                         <td>
                           <Link
                             to={`/update/${result._id}`}
@@ -247,11 +309,19 @@ const IdeaList = ({ ideaId }) => {
                     </tbody>
                   ))}
 
+                {/* //* ================ INITIAL RESULT ================ */}
+
                 {!search.length > 0 &&
                   ideas.map((idea, index) => (
                     <tbody key={index}>
                       <tr>
-                        <td>{index + 1}</td>
+                        <td>
+                          {idea.star ? (
+                            <Badge bg="warning">&#9733;</Badge>
+                          ) : (
+                            index + 1
+                          )}
+                        </td>
                         <img
                           className="rounded img-thumbnail"
                           src={idea.logo}
@@ -283,11 +353,6 @@ const IdeaList = ({ ideaId }) => {
                             </Badge>
                           ))}
                         </td>
-                        {idea.star ? (
-                          <td className="text-warning">&#9733;</td>
-                        ) : (
-                          <td></td>
-                        )}
                         <td>
                           <Link
                             to={`/update/${idea._id}`}
